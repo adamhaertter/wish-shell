712abeadfcca6910e754de6addcbd74083eece7b
Adam Haertter <ah2005@engr.ship.edu>
2022-02-21 20:48:51 -0500

    Cleaned up main before splitting

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 FILE *fp;
 char* default_path[] = {"/bin", "/user/bin"};
 char **path = default_path;
-char **args;
 
 void read_command(char* str);
 void print_error();
@@ -21,13 +20,11 @@ int main(int argc, char* argv[]) {
     int line_count;
     if(argc == 1){
         //interactive
-        while(1) { //This needs to be changed, I need a way to signal the end of the input chain
+        while(1) { 
             printf("wish> "); //terminal message
             getline(&buffer, &n, stdin);
             
             read_command(buffer);
-            
-            //printf("line%d: %s", ++line_count, buffer); //echo function
 	    if(strcmp(buffer, "exit\n") == 0){
 		    break;
 	    }
@@ -37,7 +34,6 @@ int main(int argc, char* argv[]) {
         fp = fopen(argv[1], "r");
         while(1){
             getline(&buffer, &n, fp);
-            //printf("line%d: %s", ++line_count, buffer);
             
             if(feof(fp))
                 break;
@@ -51,54 +47,28 @@ int main(int argc, char* argv[]) {
 }
 
 void read_command(char* str) {
-    char *command, *temp_string; //TODO delete temp? do we still use it?
-    //char **args; //This doesnt fix our issue
+    char *command;
+    char **args; 
     del_newline(str);
 
     command = strsep(&str," ");
     args = split_array(str);
-    /*
-    printf("command: %s | ", command);
-    if(args !=NULL)
-        printf("%s", args[0]);
-    else
-        printf("[null]");
-    printf(" | %s", *path);
-    printf("\n");
-    */
+
     //functionality
     if(strcmp(command, "exit")==0) {
         if(args != NULL)    {
-            //If exit has an argument
+            // If exit has an argument
             print_error();
         } 
-        //good exit
+        // good exit
         exit(0);   
     } else 
     if(strcmp(command, "path")==0){
-        //split arguments into path
-        //printf("path detected, assigning\n");
-        //path = args;
-        
-        /* THIS WORKS!!!!!!!!!!!
-        path[0] = malloc(100);
-        strcpy(path[0], args[0]);
-        printf("path assigned\n");
-        */
-        
-        //args = NULL;
-        // The memory allocated in split_array is being freed at the end of this run,
-        // so we can't store it in
-        //path = realloc(path, 100);
-        //free(path);
+        // split arguments into path
         if(args == NULL) {
-            //printf("I see args is null, attempting to set path[0]\n");
             path[0] = NULL;
-            //printf("path[0] now NULL\n");
         } else {
-                //printf("path loop ");
-            for(int i = 0; i < 10; i++) {            
-                //printf("%d ", i);
+            for(int i = 0; i < 10; i++) {    
                 if(args[i] == NULL) {
                     path[i] = NULL;
                     break;
@@ -108,26 +78,18 @@ void read_command(char* str) {
                 if(args[i] == NULL)
                     break;
             }
-            //printf("\n");
         }
-
-        //printf("new path: %s \n", path[0]);
         return;
     } else 
     if(strcmp(command, "cd")==0){
-        //custom cd call
+        // custom cd call
         if(args == NULL || args[1] != NULL)
             print_error();
         chdir(args[0]);
         return;
-    } /*else 
-    if(strcmp(command, "ls") == 0) {
-        path[0] = "/bin/ls";
-    } */
-    //TODO access checks
+    }
 
-    //printf("pre-command build check-in\n");
-
+    // Building full list of command + args to run
     char* exec_arr[10];
     
     exec_arr[0] = command;
@@ -141,40 +103,34 @@ void read_command(char* str) {
     }
     exec_arr[i+1] = NULL;
 
+    // Search directories for access
     char* exec_str = malloc(100);
-    if(path[0] != NULL) {
-        strcpy(exec_str, path[0]);
-        strcat(exec_str, "/");
-    }
-    else
-        strcpy(exec_str, "");
-    strcat(exec_str, command);
-    //printf("EXEC_STR: %s\n", exec_str);
-    
-    //printf("access val %d\n", access(command, X_OK));
+    for(i = 0; i < 10; i++) {
+        if(path[i] != NULL) {
+            strcpy(exec_str, path[i]);
+            strcat(exec_str, "/");
+        } else
+            strcpy(exec_str, "");
+        strcat(exec_str, command);
 
-    //printf("pre-fork check-in\n");
+        if(!access(exec_str, X_OK))
+            break;
+    }
+
     int result = 0;
     int pid = fork();
     if(pid == 0){
-        //kiddie pool
-        result = execv(exec_str, exec_arr); //TODO FIX THIS, SEARCH ALL DIRS
-        //printf("Error caught w/ value %s\n", strerror(errno));
+        // Child process
+        result = execv(exec_str, exec_arr);
         if(result)
             print_error();
         
     } else {
-        //adult swim
+        // Parent process
         waitpid(pid, 0, 0);
         free(exec_str);
         return;
     }
-    //printf("execv results in: %d", result);
-    //exit(0);
-        //printf("Hi, I am %d and I'm causing problems w/ value %s\n", pid, strerror(errno));
-        //if(errno != 0)
-        //    print_error();
-    //TODO
 }
 
 void print_error() {
